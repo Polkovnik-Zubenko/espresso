61b8df24dbf65af872ee046e2f9aa4dc76b8f3fe
sasha <kksashsash@gmail.com>
2021-12-12 23:59:15 +0300

    Капучино

--- main.py
+++ main.py
@@ -1,38 +1,192 @@
 import sqlite3
 import sys
 from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication
-from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem
+from PyQt5 import QtCore, QtGui, QtWidgets
 
 
-class DBSample(QMainWindow):
+class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('main.ui', self)
-        self.connection = sqlite3.connect("espresso.db")
-        cur = self.connection.cursor()
-        self.result = cur.execute("""SELECT * FROM ingredient""")
-        self.pushButton.clicked.connect(self.func)
-        self.pushButton.setText('Загрузить')
-        self.setWindowTitle('Эспрессо')
+        uic.loadUi("addEditCoffeeForm.ui", self)
+        self.con = sqlite3.connect("espresso.db")
+        self.pushButton_3.clicked.connect(self.update_result)
+        self.tableWidget.itemChanged.connect(self.item_changed)
+        self.pushButton_2.clicked.connect(self.save_results)
+        self.pushButton_2.setText('Сохранить')
+        self.pushButton.clicked.connect(self.onClicked_func)
+        self.modified = {}
+        self.titles = None
 
-    def func(self):
-        self.tableWidget.setColumnCount(7)
+    def update_result(self):
+        cur = self.con.cursor()
+        result = cur.execute("SELECT * FROM ingredient WHERE id=?",
+                             (item_id := self.lineEdit.text(),)).fetchall()
+        self.tableWidget.setRowCount(len(result))
+        if not result:
+            self.statusBar().showMessage('Ничего не нашлось')
+            return
+        else:
+            self.statusBar().showMessage(f"Нашлась запись с id = {item_id}")
+        self.tableWidget.setColumnCount(len(result[0]))
+        self.titles = [description[0] for description in cur.description]
         self.tableWidget.setHorizontalHeaderLabels(['id', 'Название сорта', 'Степень обжарки',
                                                     'Молотый/в зернах', 'Описание вкуса', 'Цена', 'Объем упаковки'])
-        for i, row in enumerate(self.result):
-            self.tableWidget.setRowCount(
-                self.tableWidget.rowCount() + 1)
-            for j, elem in enumerate(row):
-                self.tableWidget.setItem(
-                    i, j, QTableWidgetItem(str(elem)))
+        for i, elem in enumerate(result):
+            for j, val in enumerate(elem):
+                self.tableWidget.setItem(i, j, QTableWidgetItem(str(val)))
+        self.modified = {}
 
-    def closeEvent(self, event):
-        self.connection.close()
+    def item_changed(self, item):
+        self.modified[self.titles[item.column()]] = item.text()
+
+    def save_results(self):
+        if self.modified:
+            print(self.modified)
+            cur = self.con.cursor()
+            que = "UPDATE ingredient SET\n"
+            que += ", ".join([f"{key} = '{self.modified.get(key)}'"
+                              for key in self.modified.keys()])
+            que += " WHERE id = ?"
+            print(que)
+            cur.execute(que, (self.lineEdit.text(),))
+            self.con.commit()
+            self.modified.clear()
+
+    def onClicked_func(self):
+        self.stat_class = MyWidget2()
+        self.stat_class.show()
 
 
-if __name__ == '__main__':
+class Ui_SmallWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(555, 611)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.verticalLayoutWidget_2 = QtWidgets.QWidget(self.centralwidget)
+        self.verticalLayoutWidget_2.setGeometry(QtCore.QRect(10, 10, 511, 431))
+        self.verticalLayoutWidget_2.setObjectName("verticalLayoutWidget_2")
+        self.verticalLayout_2 = QtWidgets.QVBoxLayout(self.verticalLayoutWidget_2)
+        self.verticalLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.horizontalLayout_3 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_3.setObjectName("horizontalLayout_3")
+        self.label_2 = QtWidgets.QLabel(self.verticalLayoutWidget_2)
+        self.label_2.setObjectName("label_2")
+        self.horizontalLayout_3.addWidget(self.label_2)
+        self.lineEdit_3 = QtWidgets.QLineEdit(self.verticalLayoutWidget_2)
+        self.lineEdit_3.setObjectName("lineEdit_3")
+        self.horizontalLayout_3.addWidget(self.lineEdit_3)
+        self.verticalLayout_2.addLayout(self.horizontalLayout_3)
+        self.horizontalLayout_4 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_4.setObjectName("horizontalLayout_4")
+        self.label_3 = QtWidgets.QLabel(self.verticalLayoutWidget_2)
+        self.label_3.setObjectName("label_3")
+        self.horizontalLayout_4.addWidget(self.label_3)
+        self.lineEdit_4 = QtWidgets.QLineEdit(self.verticalLayoutWidget_2)
+        self.lineEdit_4.setObjectName("lineEdit_4")
+        self.horizontalLayout_4.addWidget(self.lineEdit_4)
+        self.verticalLayout_2.addLayout(self.horizontalLayout_4)
+        self.horizontalLayout_6 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_6.setObjectName("horizontalLayout_6")
+        self.label_5 = QtWidgets.QLabel(self.verticalLayoutWidget_2)
+        self.label_5.setObjectName("label_5")
+        self.horizontalLayout_6.addWidget(self.label_5)
+        self.lineEdit = QtWidgets.QLineEdit(self.verticalLayoutWidget_2)
+        self.lineEdit.setObjectName("lineEdit")
+        self.horizontalLayout_6.addWidget(self.lineEdit)
+        self.verticalLayout_2.addLayout(self.horizontalLayout_6)
+        self.horizontalLayout_7 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_7.setObjectName("horizontalLayout_7")
+        self.label_6 = QtWidgets.QLabel(self.verticalLayoutWidget_2)
+        self.label_6.setObjectName("label_6")
+        self.horizontalLayout_7.addWidget(self.label_6)
+        self.lineEdit_6 = QtWidgets.QLineEdit(self.verticalLayoutWidget_2)
+        self.lineEdit_6.setObjectName("lineEdit_6")
+        self.horizontalLayout_7.addWidget(self.lineEdit_6)
+        self.verticalLayout_2.addLayout(self.horizontalLayout_7)
+        self.horizontalLayout_8 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_8.setObjectName("horizontalLayout_8")
+        self.label_7 = QtWidgets.QLabel(self.verticalLayoutWidget_2)
+        self.label_7.setObjectName("label_7")
+        self.horizontalLayout_8.addWidget(self.label_7)
+        self.lineEdit_7 = QtWidgets.QLineEdit(self.verticalLayoutWidget_2)
+        self.lineEdit_7.setObjectName("lineEdit_7")
+        self.horizontalLayout_8.addWidget(self.lineEdit_7)
+        self.verticalLayout_2.addLayout(self.horizontalLayout_8)
+        self.horizontalLayout_5 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_5.setObjectName("horizontalLayout_5")
+        self.label_4 = QtWidgets.QLabel(self.verticalLayoutWidget_2)
+        self.label_4.setObjectName("label_4")
+        self.horizontalLayout_5.addWidget(self.label_4)
+        self.lineEdit_5 = QtWidgets.QLineEdit(self.verticalLayoutWidget_2)
+        self.lineEdit_5.setObjectName("lineEdit_5")
+        self.horizontalLayout_5.addWidget(self.lineEdit_5)
+        self.verticalLayout_2.addLayout(self.horizontalLayout_5)
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(420, 460, 101, 51))
+        self.pushButton.setObjectName("pushButton")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(10, 470, 201, 21))
+        self.label.setObjectName("label")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 555, 26))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.label_2.setText(_translate("MainWindow", "Название сорта    "))
+        self.label_3.setText(_translate("MainWindow", "Степень обжарки "))
+        self.label_5.setText(_translate("MainWindow", "Молотый/в зернах"))
+        self.label_6.setText(_translate("MainWindow", "Описание вкуса    "))
+        self.label_7.setText(_translate("MainWindow", "Цена                    "))
+        self.label_4.setText(_translate("MainWindow", "Объем упаковки    "))
+        self.pushButton.setText(_translate("MainWindow", "Добавить"))
+        self.label.setText(_translate("MainWindow", "TextLabel"))
+
+
+class MyWidget2(QMainWindow, Ui_SmallWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.setWindowTitle('Добавить элемент')
+        self.pushButton.clicked.connect(self.start)
+        self.con = sqlite3.connect('espresso.db')
+        self.dict = {}
+        self.counter = 0
+        self.label.setText('')
+
+    def start(self):
+        try:
+            with sqlite3.connect('espresso.db') as con:
+                cur_ = con.cursor()
+                if self.lineEdit_3.text() != '' and self.lineEdit_4.text() != '' and self.lineEdit_5.text() != '' and \
+                        self.lineEdit_6.text() != '' and self.lineEdit_7.text() != '' and self.lineEdit.text() != '':
+                    cur_.execute("""INSERT INTO ingredient(name_sort, Step_obg, grind_grains, taste_discription, 
+                    Price, Packing_volume) VALUES(?, ?, ?, ?, ?, ?)""", (self.lineEdit_3.text(),
+                                                                         self.lineEdit_4.text(),
+                                                                         self.lineEdit.text(),
+                                                                         self.lineEdit_6.text(),
+                                                                         self.lineEdit_7.text(),
+                                                                         self.lineEdit_5.text()))
+                else:
+                    self.label.setText('Неверный формат')
+        except Exception as e:
+            print(e)
+
+
+if __name__ == "__main__":
     app = QApplication(sys.argv)
-    ex = DBSample()
-    ex.show()
+    wnd = MyWidget()
+    wnd.show()
     sys.exit(app.exec())
